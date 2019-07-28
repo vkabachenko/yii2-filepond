40649e2e095750f7436bdbd4e07691a9cf5e5b2e
Viktor Kabachenko <vkabachenko@gmail.com>
2019-07-28 17:50:41 +0300

    initial

---
+++ Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace vkabachenko\filepond;
+
+/**
+ * filepond module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * {@inheritdoc}
+     */
+    public $controllerNamespace = 'vkabachenko\filepond\controllers';
+
+    /**
+     * {@inheritdoc}
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ controllers/MainController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace app\modules\filepond\controllers;
+
+use yii\rest\Controller;
+
+/**
+ * Default controller for the `filepond` module
+ */
+class MainController extends Controller
+{
+    public function actionProcess()
+    {
+        return [];
+    }
+}
